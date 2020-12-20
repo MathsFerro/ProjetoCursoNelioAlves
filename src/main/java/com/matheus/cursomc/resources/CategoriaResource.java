75de83db4efb52602940e2f183e62d229ea09540
Matheus Ferro <matheusferroraimundo@gmail.com>
2020-12-20 17:14:01 -0300

    adicionando categoria

---
+++ src/main/java/com/matheus/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,19 @@
+package com.matheus.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+	private Integer id;
+	private String nome;
+	 
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest funcionando";
+	}
+}
