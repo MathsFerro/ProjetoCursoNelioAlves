248e66f6e2793cfb5a65bff8281e19fa856cc03c
Matheus Ferro <matheusferroraimundo@gmail.com>
2020-12-20 23:58:27 -0300

    criando repository e service para categoria

--- src/main/java/com/matheus/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/matheus/cursomc/resources/CategoriaResource.java
@@ -3,27 +3,26 @@ package com.matheus.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheus.cursomc.domain.Categoria;
+import com.matheus.cursomc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		Categoria c1 = new Categoria(1, "Informática");
-		Categoria c2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> listCategoria = new ArrayList();
-		
-		listCategoria.add(c1);
-		listCategoria.add(c2);
-		
-		return listCategoria;
+	@Autowired // Instanciar automaticamente
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find( @PathVariable Integer id  ) {
+		return ResponseEntity.ok().body(service.buscar(id));
 	}
 }
---
+++ src/main/java/com/matheus/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.matheus.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.matheus.cursomc.domain.Categoria;
+import com.matheus.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired // É automaticamente instanciado pelo spring
+	private CategoriaRepository repository;
+	
+	public Categoria buscar( Integer id ) {
+		return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Não encontrado"));
+	}
+	
+}
